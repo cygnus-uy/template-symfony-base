1d47837974c99e2170915f23eed12ab5c8e8ff66
samuel <samuel@cygnus.com.uy>
2022-10-17 17:56:28 -0300

    -> Instalación y configuración de tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./app/website/**/*.js', './templates/**/*.html.twig'],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+}
