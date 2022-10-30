95d0bdc8cde7c8e4c5ce073e52c1941b5ee93edf
Samuel <samuel@cygnus.com.uy>
2022-10-30 09:10:41 -0300

    -> Instalación de nelmio/cors-bundle con su configuración por defecto.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Nelmio\SecurityBundle\NelmioSecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
